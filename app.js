ce44d3293adbc6ae89e243d2736d6c4c0c94e077
Mukeshkhanna11 <mukeshmass.9865@gmail.com>
2023-12-09 23:51:51 +0530

    update promises

--- app.js
+++ app.js
@@ -1,27 +1,30 @@
-const { readFile } = require("fs");
-const getText = (path) => {
-  return new Promise((reject, resolve) => {
-    readFile(path, "utf8", (err, result) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(result);
-      }
-    });
-  });
-};
-//
-
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 const start = async () => {
   try {
-    const first = await getText("./content/first.txt");
-
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
+    await writeFilePromise("./content/third.txt", "this is third file");
     console.log(first, second);
   } catch (error) {
     console.log(error);
   }
 };
 start();
+
+// const getText = (path) => {
+//   return new Promise((reject, resolve) => {
+//     readFile(path, "utf8", (err, result) => {
+//       if (err) {
+//         reject(err);
+//       } else {
+//         resolve(result);
+//       }
+//     });
+//   });
+//
 //getText("./content/first.txt")
 //   .then((result) => console.log(result))
 //   .catch((err) => console.log(err));
